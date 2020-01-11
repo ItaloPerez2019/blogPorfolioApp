60db17bbb7692489536a224727f9f8092a3856e5
Italo Perez <pyitalo@gmail.com>
2020-01-11 17:02:12 -0500

    modifications on models

--- porfolio/settings.py
+++ porfolio/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'porfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'porfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'g00147441',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
